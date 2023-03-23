c84e0c55563f8ebcd9402559cd84a3bc1fccd364
prettyblueberry <prettyblueberry52702@gmail.com>
2023-03-23 13:37:54 +0900

    client usermenu by role

--- client/src/components/Header/UserMenu.js
+++ client/src/components/Header/UserMenu.js
@@ -1,7 +1,12 @@
 import {Link} from "react-router-dom";
 
 const LoginButton = <Link to='/authentication/sign-in/basic'>Login</Link>
-const submenus = [
+const submenus4Normal = [
+    <Link to="/pages/user/profile">Profile</Link>,
+    <Link to="/sign-out">Logout</Link>
+];
+
+const submenus4Admin = [
     <Link to="/pages/user/profile">Profile</Link>,
     <Link to="/admin">Admin Panel</Link>,
     <Link to="/sign-out">Logout</Link>
@@ -12,6 +17,7 @@ const UserName = ({ auth }) =>
 
 const useAuth = () => {
     const auth = localStorage.getItem("auth");
+    let submenus;
     let Title = null;
     let isLogin = false;
     if(auth === null){
@@ -20,11 +26,14 @@ const useAuth = () => {
         Title = <UserName auth={auth} />;
         isLogin = true;
     }
-    return [isLogin, Title];
+    if(auth.user.role === "admin") submenus = submenus4Admin;
+    else submenus = submenus4Normal;
+
+    return [isLogin, Title, submenus];
 }
 
 export function UserMenuMobile(){
-    const [isLogin, Title] = useAuth();
+    const [isLogin, Title, submenus] = useAuth();
     return <li className="
               menu-item
               menu-item-type-post_type
